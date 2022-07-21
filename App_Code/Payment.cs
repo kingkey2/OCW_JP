a050ca3f2de1330bca7018f3cd2e68d9aa785df9
eric <a9725051@gmail.com>
2022-07-21 17:58:26 +0800

    1

--- App_Code/Payment.cs
+++ App_Code/Payment.cs
@@ -167,6 +167,197 @@ public class Payment {
             }
         }
 
+        public static APIResult CreateEPayDeposite(string OrderID, decimal OrderAmount, string Type, string UserName,string ContactPhoneNumber)
+        {
+            APIResult R = new APIResult() { ResultState = APIResult.enumResultCode.ERR };
+            JObject sendData = new JObject();
+            string URL;
+            string ReturnURL;
+            string Path;
+            string CompanyCode;
+            string CurrencyType;
+            string CompanyKey;
+            string ServiceType;
+            DateTime OrderDate = DateTime.Now;
+            string Sign;
+            string result;
+            System.Data.DataTable DT = new System.Data.DataTable();
+            string token = EWinWeb.EPayToken;
+            decimal JPYRate = 0;
+            dynamic EPAYSetting = null;
+            JObject returnResult;
+
+            if (EWinWeb.IsTestSite)
+            {
+                Path = HttpContext.Current.Server.MapPath("/App_Data/EPay/Test_" + SettingFile);
+            }
+            else
+            {
+                Path = HttpContext.Current.Server.MapPath("/App_Data/EPay/Formal_" + SettingFile);
+            }
+
+            if (System.IO.File.Exists(Path))
+            {
+                string SettingContent;
+
+                SettingContent = System.IO.File.ReadAllText(Path);
+
+                if (string.IsNullOrEmpty(SettingContent) == false)
+                {
+                    try { EPAYSetting = Newtonsoft.Json.JsonConvert.DeserializeObject(SettingContent); } catch (Exception ex) { }
+                }
+            }
+
+            if (EPAYSetting == null)
+            {
+                R.Message = "Get EPaySetting Fail";
+                return R;
+            }
+
+            URL = (string)EPAYSetting.ApiUrl + "RequirePayingReturnUrl";
+            if (Type == "EPayJKC")
+            {
+                DT = RedisCache.PaymentMethod.GetPaymentMethodByCategory("EPAYJKC");
+                var MultiCurrencyInfo = (string)DT.Select("PaymentCategoryCode='" + "EPAYJKC" + "'")[0]["MultiCurrencyInfo"];
+                Newtonsoft.Json.Linq.JArray MultiCurrency = Newtonsoft.Json.Linq.JArray.Parse(MultiCurrencyInfo);
+                for (int i = 0; i < MultiCurrency.Count; i++)
+                {
+                    if ((string)MultiCurrency[i]["Currency"] == "JPY")
+                    {
+                        JPYRate = (decimal)MultiCurrency[i]["Rate"];
+                    }
+                }
+                ReturnURL = EWinWeb.CasinoWorldUrl + "/Payment/EPay/JKCPaymentCallback.aspx";
+            }
+            else
+            {
+                ReturnURL = EWinWeb.CasinoWorldUrl + "/Payment/EPay/PaymentCallback.aspx";
+            }
+
+            CompanyCode = (string)EPAYSetting.CompanyCode;
+            CurrencyType = (string)EPAYSetting.CyrrencyType;
+            CompanyKey = (string)EPAYSetting.ApiKey;
+            ServiceType = (string)EPAYSetting.ServiceType;
+            sendData.Add("ManageCode", CompanyCode);
+            sendData.Add("Currency", CurrencyType);
+            sendData.Add("Service", ServiceType);
+            sendData.Add("CustomerIP", CodingControl.GetUserIP());
+            sendData.Add("OrderID", OrderID);
+            sendData.Add("OrderDate", OrderDate.ToString("yyyy-MM-dd HH:mm:ss"));
+            if (Type == "EPayJKC")
+            {
+                Sign = GetEPayDepositSign(OrderID, OrderAmount * JPYRate, OrderDate, ServiceType, CurrencyType, CompanyCode, CompanyKey);
+                sendData.Add("OrderAmount", (OrderAmount * JPYRate).ToString("#.##"));
+            }
+            else
+            {
+                Sign = GetEPayDepositSign(OrderID, OrderAmount, OrderDate, ServiceType, CurrencyType, CompanyCode, CompanyKey);
+                sendData.Add("OrderAmount", OrderAmount.ToString("#.##"));
+            }
+
+            sendData.Add("RevolveURL", ReturnURL);
+            sendData.Add("UserName", UserName);
+            sendData.Add("State", ContactPhoneNumber);
+            sendData.Add("Sign", Sign);
+            
+            using (HttpClientHandler handler = new HttpClientHandler())
+            {
+                using (HttpClient client = new HttpClient(handler))
+                {
+                    try
+                    {
+                        #region 呼叫遠端 Web API
+
+                        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, URL);
+                        HttpResponseMessage response = null;
+
+                        #region  設定相關網址內容
+
+                        // Accept 用於宣告客戶端要求服務端回應的文件型態 (底下兩種方法皆可任選其一來使用)
+                        //client.DefaultRequestHeaders.Accept.TryParseAdd("application/json");
+                        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        client.DefaultRequestHeaders.TryAddWithoutValidation("token", token);
+                        // Content-Type 用於宣告遞送給對方的文件型態
+                        //client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+
+
+                        // 將 data 轉為 json
+                        string json = sendData.ToString();
+                        request.Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+                        response = client.SendAsync(request).GetAwaiter().GetResult();
+
+                        #endregion
+                        #endregion
+
+                        #region 處理呼叫完成 Web API 之後的回報結果
+                        if (response != null)
+                        {
+                            if (response.IsSuccessStatusCode == true)
+                            {
+                                // 取得呼叫完成 API 後的回報內容
+                                result = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                            }
+                            else
+                            {
+                                result = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                            }
+                        }
+                        else
+                        {
+                            R.Message = "Create Order Fail";
+                            return R;
+                        }
+                        #endregion
+
+                    }
+                    catch (Exception ex)
+                    {
+                        R.Message = "Create Order Fail";
+                        return R;
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(result))
+            {
+                returnResult = Newtonsoft.Json.JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(result);
+                if (returnResult["Status"].ToString() == "0")
+                {
+                    R.ResultState = APIResult.enumResultCode.OK;
+                    R.Message = returnResult["Message"].ToString();
+                    return R;
+                }
+                else
+                {
+                    R.Message = "Create Order Fail";
+                    return R;
+                }
+            }
+            else
+            {
+                R.Message = "Send to EPay Fail";
+                return R;
+            }
+        }
+
+        public static string GetEPayDepositSign(string OrderID, decimal OrderAmount, DateTime OrderDateTime, string ServiceType, string CurrencyType, string CompanyCode, string CompanyKey)
+        {
+            string sign;
+            string signStr = "ManageCode=" + CompanyCode;
+            signStr += "&Currency=" + CurrencyType;
+            signStr += "&Service=" + ServiceType;
+            signStr += "&OrderID=" + OrderID;
+            signStr += "&OrderAmount=" + OrderAmount.ToString("#.##");
+            signStr += "&OrderDate=" + OrderDateTime.ToString("yyyy-MM-dd HH:mm:ss");
+            signStr += "&CompanyKey=" + CompanyKey;
+
+            sign = CodingControl.GetSHA256(signStr, false).ToUpper();
+
+            return sign;
+        }
+
         private static string GetEPayWithdrawSign(string OrderID, decimal OrderAmount, DateTime OrderDateTime, string CurrencyType, string CompanyCode, string CompanyKey)
         {
             string sign;
